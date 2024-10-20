8902d841917f1a6730774664314e18946d2437bc
akan-baba <akan_antia@live.co.uk>
2024-10-20 14:57:51 +0100

    Jewellery

---
+++ Test Suites/2. ShoppingCart/TS06_Jewellery.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS06_Jewellery</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>50</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>bf1ee907-5471-4d2f-907e-f0254dbd699b</testSuiteGuid>
+   <testCaseLink>
+      <guid>12a63149-ee7c-4835-9e37-c82a75210e09</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2-Shopping-Basket/4. Jewellery/a.create-Own-jewellery/39.create-jewellery-checkout-as-guest-creditCard</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>22565f5a-699b-4a6c-88d3-49306200899a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2-Shopping-Basket/4. Jewellery/a.create-Own-jewellery/40.create-jewellery-checkout-as-guest-moneyOrder</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bbb113d4-e1f8-4fb3-a602-beca841b7735</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2-Shopping-Basket/4. Jewellery/a.create-Own-jewellery/41.create-jewellery-newRegister-creditCard</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>228e3673-b014-4481-badf-f8a68f4bac86</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2-Shopping-Basket/4. Jewellery/a.create-Own-jewellery/42.create-jewellery-newRegister-moneyOrder</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
